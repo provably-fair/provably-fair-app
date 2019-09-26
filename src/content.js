f685547cf71159372dcc0404e685024a5c29df93
devilla <dev.koold@gmail.com>
2019-09-26 17:06:04 +0530

    Get started size fix

--- src/content.js
+++ src/content.js
@@ -928,7 +928,7 @@ handleVerifyBetForLimbo = (serverSeedHash,clientSeed, nonce) => {
                            <div style={{display:gettingStarted?'block':'none'}}>
 
                             <h6 className="text-center"><strong>Crypto Gambling Foundation Verifier</strong></h6>
-                            <img src="https://pbs.twimg.com/profile_images/906057204578390016/-icT77rY_400x400.jpg" />
+                            <img src="https://pbs.twimg.com/profile_images/906057204578390016/-icT77rY_400x400.jpg"  style={{ width: '90%'}}/>
 
                             <p><span style={{fontStyle:'bold'}}>Operator</span> is a CGF verified operator.</p>
                             <button className="btn btn-info mb-3" type="button" onClick={()=>{
